7693697098aacfa59f20de58428c0ba10095cad7
glipski <grzegorzjakublipski@gmail.com>
2017-04-10 22:52:25 +0200

    jeszcze raz

--- kzk/settings.py
+++ kzk/settings.py
@@ -157,3 +157,9 @@ STATICFILES_DIRS = (
 )
 
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
