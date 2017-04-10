06ead275cde19ce24ee454ff5d3af621323cfff4
glipski <grzegorzjakublipski@gmail.com>
2017-04-10 23:55:56 +0200

    23

--- kzk/settings.py
+++ kzk/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'kzk.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite')
+'default': dj_database_url.config(default='sqlite:///db.sqlite')
 }
 
 
