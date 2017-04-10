81106313d0fedc216e462b552f19cfde98bb1c67
glipski <grzegorzjakublipski@gmail.com>
2017-04-10 22:51:09 +0200

    jeszcze raz

--- kzk/settings.py
+++ kzk/settings.py
@@ -15,7 +15,7 @@ import os
 #
 
 import dj_database_url
-#db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 
@@ -33,7 +33,7 @@ SECRET_KEY = 'jv4vvk_+4s5_jw_k12gmx7hkrx%vyilnh=%j_yxgrvuaqu)9n^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -143,9 +143,8 @@ REGISTRATION_AUTO_LOGIN = True
 LOGIN_REDIRECT_URL = '/projekty/'
 LOGIN_URL = '/konta/login/'
 
-
-
-#
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -153,6 +152,8 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = (
-#    os.path.join(PROJECT_ROOT, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
