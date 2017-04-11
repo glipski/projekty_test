848e3898be1fa4808ea0a9d5c2d890a1ae7520e7
glipski <grzegorzjakublipski@gmail.com>
2017-04-11 09:22:54 +0200

    25

--- kzk/settings.py
+++ kzk/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jv4vvk_+4s5_jw_k12gmx7hkrx%vyilnh=%j_yxgrvuaqu)9n^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['projekty-kzk.herokuapp.com']
 
