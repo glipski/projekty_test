4348fb4a32d7f4b0e27b8f081e0dde86a995d4dd
glipski <grzegorzjakublipski@gmail.com>
2017-04-11 11:47:25 +0200

    commit 26

--- kzk/settings.py
+++ kzk/settings.py
@@ -40,9 +40,7 @@ SECRET_KEY = 'jv4vvk_+4s5_jw_k12gmx7hkrx%vyilnh=%j_yxgrvuaqu)9n^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['projekty-kzk.herokuapp.com', 
-                 '127.0.0.1', 
-                 'infinite-fortress-1562.herokuapp.com']
+ALLOWED_HOSTS = ['projekty-kzk.herokuapp.com', '127.0.0.1', 'infinite-fortress-1562.herokuapp.com']
 
 
 # Application definition
