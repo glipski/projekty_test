336373cd18c9f9209b8265a9577ca733e1e3a7d6
glipski <grzegorzjakublipski@gmail.com>
2017-04-13 11:29:51 +0200

    36

--- kzk/settings.py
+++ kzk/settings.py
@@ -99,7 +99,8 @@ WSGI_APPLICATION = 'kzk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+#        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydatabase',
     }
 }
